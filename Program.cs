049bf807138c2d5dac6b070cdb0fccec4ba54d7a
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-02-06 06:27:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MyConsoleApp;
+
+internal sealed class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var host = CreateHostBuilder(args).Build();
+        await host.RunAsync();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                // Add ApplicationInsights.config (see https://stackoverflow.com/questions/54772078/application-insights-from-last-debug-session)
+                services.AddApplicationInsightsTelemetryWorkerService();
+
+                // Register our background worker that uses Application Insights.
+                services.AddHostedService<Worker>();
+            });
+}
+
+
+internal sealed class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> logger;
+    private readonly TelemetryClient telemetryClient;
+
+    public Worker(ILogger<Worker> logger, TelemetryClient telemetryClient)
+    {
+        this.logger          = logger;
+        this.telemetryClient = telemetryClient;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        logger.LogInformation("Worker started at: {time}", DateTimeOffset.Now);
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var message = $"Heartbeat at {DateTimeOffset.Now}";
+            logger.LogInformation(message);
+
+            // Send a trace message to Application Insights.
+            telemetryClient.TrackTrace(message);
+
+            // Wait 5 seconds before sending the next telemetry item.
+            await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+        }
+    }
+}
